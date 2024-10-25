8f48c8509f930665aabf5ddd1b24a57199ae5dbb
Claudia Perez <perezvelazquezclaudia@gmail.com>
2024-10-25 13:46:50 -0300

    primer push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import './index.css'
+import Categories from './views/generalViews/Categories'
 
 function App() {
 
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-    <h1 className="text-3xl font-bold text-blue-500">¡Comenzando PROYECTO FINAL!</h1>
-  </div>
+    <div className="">
+      <Categories/>
+    </div>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        naranja: '#C95723', // paprika
+        amarillo: '#F5AF33', // tumeric
+        limon: '#C8C346', // lemongrass
+        verde: '#1F532F', // caribben
+        tierra: '#705D63', // caribben
+        naranjal: '#FFE7DC', // paprika-light
+        amarillol: '#FFF2DB', // tumeric-light
+        limonl: '#FFFC8F', // lemongrass-light
+        verdel: '#C5FFD7', // caribben-light
+        tierral: '#DDDDDD', // caribben-light
+      },
+    },
   },
   plugins: [],
 }
