76dc2b44a135fb6f1d1f02d60525615202bad322
Matiasalbina <154567225+Matiasalbina@users.noreply.github.com>
2024-10-25 20:22:58 -0300

    Slidebar (carousel) actualizado

--- frontend/src/components/layouts/Sliderbar.jsx
+++ frontend/src/components/layouts/Sliderbar.jsx
@@ -1,1 +1,66 @@
+import React from 'react';
 
+const Carousel = () => {
+  return (
+    <div className="carousel rounded-box w-full h-64 mx-auto">
+      <div className="carousel-item">
+        <img
+          src="./public/images/cafe-colombia.png"
+          alt="Burger"
+          className="w-48 h-48 object-cover"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src="./public/images/cafe-colombia.png"
+          alt="Burger"
+          className="w-48 h-48 object-cover"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src="./public/images/cafe-colombia.png"
+          alt="Burger"
+          className="w-48 h-48 object-cover"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src="./public/images/cafe-colombia.png"
+          alt="Burger"
+          className="w-48 h-48 object-cover"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src="./public/images/cafe-colombia.png"
+          alt="Burger"
+          className="w-48 h-48 object-cover"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src="./public/images/cafe-colombia.png"
+          alt="Burger"
+          className="w-48 h-48 object-cover"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src="./public/images/cafe-colombia.png"
+          alt="Burger"
+          className="w-48 h-48 object-cover"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src="./public/images/cafe-colombia.png"
+          alt="Burger"
+          className="w-48 h-48 object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
