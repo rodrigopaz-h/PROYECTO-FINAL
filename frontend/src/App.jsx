9fe337bf1920b3e27f067d0b73ea384bb6d44de3
Claudia Perez <perezvelazquezclaudia@gmail.com>
2024-10-25 13:52:14 -0300

    quitar categorias de app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 import './index.css'
-import Categories from './views/generalViews/Categories'
 
 function App() {
 
 
   return (
-    <div className="">
-      <Categories/>
+    <div>
     </div>
   )
 }
