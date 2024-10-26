5362a4335ba6dd0fd9890a357c1b79950eff8069
Matiasalbina <154567225+Matiasalbina@users.noreply.github.com>
2024-10-25 21:56:48 -0300

    Rutas modificadas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
-import './App.css'
-import './index.css'
+import "./App.css";
+import "./index.css";
+import React from "react";
+import Navbar from "./components/layouts/Navbar";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Carousel from "./components/layouts/Sliderbar";
+import UserRegistration from "./views/userViews/UserRegistration";
+import UserLogin from "./views/userViews/UserLogin";
+import Hero from "./components/layouts/Hero";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/layouts/Home";
 
 function App() {
-
-
   return (
-    <div>
-    </div>
-  )
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
