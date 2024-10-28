c42bd651d3f0a9be50d75ff7947c69af7ad7bb6d
Claudia Perez <perezvelazquezclaudia@gmail.com>
2024-10-28 00:54:10 -0300

    cambios

--- frontend/src/views/generalViews/BlogSection.jsx
+++ frontend/src/views/generalViews/BlogSection.jsx
@@ -8,7 +8,7 @@ export default function BlogSection() {
     <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
       {/* Title */}
       <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-        <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">The Blog</h2>
+        <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Blog</h2>
         <p className="mt-1 text-gray-600 dark:text-neutral-400">
           See how game-changing companies are making the most of every engagement with Preline.
         </p>
--- frontend/src/views/generalViews/SectionsHome.jsx
+++ frontend/src/views/generalViews/SectionsHome.jsx
@@ -8,7 +8,7 @@ export default function SectionsHome({titulo,descripcion, tipoSeccion, fondo}) {
   return (
         <div className= {`mx-auto w-full relative pt-10 pb-10 ${fondo}`}>
             <div className="max-w-xl mx-auto text-center lg:text-balance">
-            <h2 className="text-xl leading-tight tracking-tight sm:text-2xl md:text-3xl lg:text-4xl font-semibold text-base-900 lg:text-balance">
+            <h2 className="text-xl leading-tight tracking-tight sm:text-2xl md:text-3xl lg:text-6xl font-semibold text-base-900 lg:text-balance">
                 {titulo}
             </h2>
             <p className="text-base leading-normal mt-4 text-base-500 font-medium">
