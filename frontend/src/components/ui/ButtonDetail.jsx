e01d64da0fbf2d6cde9ef77621cbee80c84f087d
Rodrigo Paz <rodrigo.pazhernandez@gmail.com>
2024-10-29 05:59:39 -0300

    ajuste

--- frontend/src/components/ui/ButtonDetail.jsx
+++ frontend/src/components/ui/ButtonDetail.jsx
@@ -19,7 +19,7 @@ const CardDetail = ({ title, description, imgSrc, authorImg, authorName, text, l
       </div>
       <button 
         onClick={handleClick} 
-        className={'mt-4 py-2 px-4 text-white font-semibold rounded-lg hover:bg-opacity-90 ${bg}} // Utiliza la propiedad bg aquí
+        className={'mt-4 py-2 px-4 text-white font-semibold rounded-lg hover:bg-opacity-90 ${bg}'}
       >
         {text}
       </button>
