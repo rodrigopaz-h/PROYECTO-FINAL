90b6a7a265f222dfa1ebdf530cadcf8e826ada77
Matiasalbina <154567225+Matiasalbina@users.noreply.github.com>
2024-11-06 14:59:36 -0300

    actualizacion controller

---
+++ backend/src/controllers/todosController.js
@@ -0,0 +1,12 @@
+import TodosModel from '../models/todosModel.js';
+
+// Obtener todos los productos (cafés y accesorios) de la vista 'todos'
+export async function getAllProducts(req, res) {
+    try {
+        const products = await TodosModel.getAllProducts();
+        res.json(products);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error al obtener los productos");
+    }
+}
