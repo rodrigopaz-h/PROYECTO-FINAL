0039d3c5078e0a4a4b37a5fb51352f03b4c2d460
Matiasalbina <154567225+Matiasalbina@users.noreply.github.com>
2024-11-07 14:36:37 -0300

    actualizacion sql

--- backend/scr/db/users.sql
+++ backend/scr/db/users.sql
@@ -1,16 +1,3 @@
--- Crear la base de datos aromacafe si no existe
-DO
-$do$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'aromacafe') THEN
-      PERFORM dblink_exec('dbname=postgres user=postgre password=Esteban23catalan27@', 'CREATE DATABASE aromacafe');
-   END IF;
-END
-$do$;
-
--- Cambiar el contexto a la base de datos aromacafe
-\c aromacafe;
-
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
@@ -19,7 +6,3 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-INSERT INTO users (first_name, last_name, email, password)
-VALUES ('Juan', 'Perez', 'juan.perez@example.com', 'hashed_password');
-
