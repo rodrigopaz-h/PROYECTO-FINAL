c8d3d597a9b682cbcee8d8b83bd83995664e484d
Claudia Perez <perezvelazquezclaudia@gmail.com>
2024-11-09 10:05:47 -0300

    cambios en cafes.js

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+import { verifyToken } from "../utils/authUtils.js";
+
+export const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Acceso denegado: se requiere un token" });
+  }
+
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token inválido o expirado" });
+  }
+};
