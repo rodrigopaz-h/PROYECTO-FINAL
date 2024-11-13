4be7688a11807e913177fce1ba047270a63aca32
Rodrigo <rodrigo.pazhernandez@gmail.com>
2024-11-13 04:09:19 -0300

    se activan rutas

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // Rutas públicas para autenticación
 router.post("/register", userController.createUser);
 router.post("/login", userController.loginUser);
-router.post("/password-reset", userController.passwordResetRequest);
+// router.post("/password-reset", userController.passwordResetRequest);
 
 // // Rutas protegidas para perfil de usuario
 router.get("/profile", authMiddleware, userController.showUserProfile);
