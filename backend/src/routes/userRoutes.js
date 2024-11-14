bb4e0826c8b874f01dc67b2c6e9ca6edcb95b121
Rodrigo <rodrigo.pazhernandez@gmail.com>
2024-11-13 23:22:39 -0300

    se revierte al original

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -5,21 +5,13 @@ import * as userController from "../controllers/userController.js";
 const router = express.Router();
 
 // Rutas públicas para autenticación
-router.post("/api/users/register", userController.createUser);
-router.post("/api/users/login", userController.loginUser);
+router.post("/register", userController.createUser);
+router.post("/login", userController.loginUser);
 // router.post("/password-reset", userController.passwordResetRequest);
 
 // // Rutas protegidas para perfil de usuario
-router.get(
-  "/api/users/profile",
-  authMiddleware,
-  userController.showUserProfile
-);
-router.put(
-  "/api/users/profile",
-  authMiddleware,
-  userController.updateUserProfile
-);
+router.get("/profile", authMiddleware, userController.showUserProfile);
+router.put("/profile", authMiddleware, userController.updateUserProfile);
 
 router.get("/", userController.getAllUsers);
 
